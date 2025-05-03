71220cb37228bc4e1f9133612a7cb91a26f20aeb
cohec <cohen.lu2000@gmail.com>
2025-05-03 13:50:28 -0700

    first commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+#include "pros/colors.hpp"
+#include "pros/optical.hpp"
+#include "pros/vision.hpp"
+
+extern Drive chassis;
+inline pros::Motor intake(4);
+inline pros::Motor hook(5);
+inline pros::Motor lb(17);
+inline pros::Optical op(2);
+inline ez::Piston goalClamp('A');
+inline ez::Piston climbArm('B');
+inline ez::Piston goalrush('C');
+inline ez::Piston ringrush('D');
+inline ez::Piston intakeLift('E');
+inline pros::Optical col(6);
