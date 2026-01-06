2b2cc47eb6025d67879165bb4a1805351deb98ad
cohec <cohen.lu2000@gmail.com>
2026-01-05 19:02:26 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -385,9 +385,9 @@ void measure_offsets() {
 // . . .
 void left_side() {
   chassis.drive_angle_set(-90);
-  chassis.pid_drive_set(27, DS, true);
+  chassis.pid_drive_set(25, DS);
   chassis.pid_wait();
-  chassis.pid_drive_set(7, DS/3);
+  chassis.pid_drive_set(10, DS/3);
   chassis.pid_wait();
   chassis.pid_turn_set(-180, TS);
   chassis.pid_wait();
@@ -395,10 +395,10 @@ void left_side() {
   intakeLS.move(127);
   intakeUS.move(-127);
   pros::delay(700);
-  chassis.pid_drive_set(8, DS/2, true);
+  chassis.pid_drive_set(8, DS/2);
   chassis.pid_wait();
   pros::delay(1000);
-  chassis.pid_drive_set(-30, DS, true);
+  chassis.pid_drive_set(-30, DS);
   chassis.pid_wait_until(-6);
   matchload.set(false);
   chassis.pid_wait();
