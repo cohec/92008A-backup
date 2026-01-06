cd3eda0de6122b85bec6ab4cd3ff1880275cd0b6
cohec <cohen.lu2000@gmail.com>
2026-01-06 15:55:33 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -386,10 +386,10 @@ void measure_offsets() {
 void left_side() {
   chassis.drive_angle_set(-90);
   chassis.odom_xyt_set(0, 0, -90);
-  chassis.pid_odom_set(25, DS, true);
+  chassis.pid_odom_set(22, DS, true);
   chassis.pid_wait_quick_chain();
   matchload.set(true);
-  chassis.pid_odom_set(11.5, DS/2);
+  chassis.pid_odom_set(10, DS/2);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(-180, TS);
   chassis.pid_wait_quick_chain();
