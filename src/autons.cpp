dd240d16c5055d41dde68991a9327f32066122b3
cohec <cohen.lu2000@gmail.com>
2026-01-06 17:38:00 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -398,7 +398,7 @@ void left_side() {
   pros::delay(300);
   chassis.pid_odom_set(10, DS/3);
   chassis.pid_wait_quick_chain();
-  pros::delay(20);
+  pros::delay(25);
   chassis.pid_odom_set(-24, DS/1.5, true);
   chassis.pid_wait_until(-6);
   matchload.set(false);
@@ -411,11 +411,11 @@ void left_side() {
   chassis.pid_odom_set({{-24, 10, 45}, fwd, DS});
   intakeUS.move(127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set(35, DS/2);
+  chassis.pid_odom_set(34, DS/2);
   intakeUS.move(-127);
   chassis.pid_wait();
   intakeLS.move(-60);
-  pros::delay(1500);
+  pros::delay(1600);
 }
 
 void right_side() {
