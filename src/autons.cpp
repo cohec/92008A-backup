6bec9e8fd50794f49faf8f9bffe5ebe44e64b1c7
cohec <cohen.lu2000@gmail.com>
2026-01-06 18:28:45 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -400,7 +400,7 @@ void left_side() {
   pros::delay(300);
   chassis.pid_odom_set(10, DS/3);
   chassis.pid_wait_quick_chain();
-  pros::delay(25);
+  pros::delay(20);
   chassis.pid_odom_set(-24, DS/1.5, true);
   chassis.pid_wait_until(-6);
   matchload.set(false);
@@ -418,7 +418,7 @@ void left_side() {
   chassis.pid_wait();
   intakeLS.move(-60);
   pros::delay(1600);
-  chassis.pid_odom_set({{-20, 10, 0}, rev, DS});
+  chassis.pid_odom_set({{-20, 0, 0}, rev, DS});
   chassis.pid_wait_quick_chain();
 }
 
