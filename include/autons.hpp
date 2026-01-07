22665399d0ffdb7e6e2653041a0bbba48f78e8a4
cohec <cohen.lu2000@gmail.com>
2026-01-07 00:32:45 -0800

    Update autons.hpp

--- include/autons.hpp
+++ include/autons.hpp
@@ -13,6 +13,6 @@ void odom_pure_pursuit_wait_until_example();
 void odom_boomerang_example();
 void odom_boomerang_injected_pure_pursuit_example();
 void measure_offsets();
-void left_side();
-void right_side();
+void left_side_elims();
+void right_side_elims();
 void skills();
