be837c9d2c23f57b25e01996cdca25c5fa799a12
cohec <cohen.lu2000@gmail.com>
2026-01-07 20:37:23 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -446,7 +446,7 @@ void left_side_elims() {
   descore.set(true);
   chassis.drive_angle_set(-90);
   chassis.odom_xyt_set(0, 0, -90);
-  chassis.pid_odom_set(28, DS, true);
+  chassis.pid_odom_set(29, DS, true);
   chassis.pid_wait_until(10);
   matchload.set(true);
   chassis.pid_wait_quick_chain();
@@ -456,7 +456,7 @@ void left_side_elims() {
   intakeUS.move(-127);
   chassis.pid_odom_set(10, DS/3);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set(-22, DS, true);
+  chassis.pid_odom_set(-24, DS, true);
   chassis.pid_wait_until(-6);
   matchload.set(false);
   chassis.pid_wait_quick_chain();
@@ -475,8 +475,8 @@ void left_side_elims() {
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(-135, TS);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set(-18, DS/2);
-  chassis.pid_wait_until(-14);
+  chassis.pid_odom_set(-14, DS/2);
+  chassis.pid_wait_until(-10);
   trapdoor.set(true);
   chassis.pid_wait_quick_chain();
   pros::delay(1500);
