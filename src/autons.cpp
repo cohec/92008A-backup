33fe42e836fdb4990076da348d260c5716495c70
cohec <cohen.lu2000@gmail.com>
2026-01-08 17:28:04 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -433,6 +433,8 @@ void auto_wp_quals() {
   chassis.pid_odom_set({{-40, 27, -90}, fwd, DS});
   intakeLS.move(127);
   chassis.pid_wait_quick_chain();
+  chassis.pid_odom_set(-0.5, DS);
+  chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(-135, TS);
   chassis.pid_wait_quick_chain();
   chassis.pid_odom_set(-18, DS/2);
