a593be249afe86e6bc54df7abdbe8b01ad5acafa
cohec <cohen.lu2000@gmail.com>
2026-01-10 01:35:57 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -617,8 +617,8 @@ void skills() {
   intakeLS.move(127);
   intakeUS.move(-127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(-5, DS/2);
   matchload.set(true);
+  chassis.pid_drive_set(-2, DS/2);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(45, TS);
   chassis.pid_wait_quick_chain();
@@ -665,5 +665,19 @@ void skills() {
   pros::delay(2000);
   intakeUS.move(0);
   chassis.pid_odom_set(5, DS);
+  chassis.pid_wait_quick_chain();
+  chassis.pid_odom_set({{20, 24, -45}, fwd, DS});
+  intakeUS.move(127);
+  chassis.pid_wait_quick_chain();
+  chassis.pid_odom_set(7, DS/2);
+  intakeUS.move(-127);
+  chassis.pid_wait_quick_chain();
+  intakeLS.move(-100);
+  pros::delay(2000);
+  chassis.pid_odom_set(-3, DS);
+  chassis.pid_wait_quick_chain();
+  chassis.pid_odom_set({{0, 10, 180}, fwd, DS});
+  intakeLS.move(127);
+  intakeUS.move(127);
   chassis.pid_wait_quick_chain();
 }
