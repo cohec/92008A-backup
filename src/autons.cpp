e3a3cba14844938d99fac4581ac88027f64089c1
cohec <cohen.lu2000@gmail.com>
2026-01-10 12:57:24 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -552,7 +552,7 @@ void skills() {
   matchload.set(true);
   chassis.pid_odom_set(5, DS/1.5);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set({{-15, 32, -135}, rev, 127});
+  chassis.pid_odom_set({{-15, 31, -135}, rev, 127});
   chassis.pid_wait_quick_chain();
   chassis.pid_odom_set(-5.5, DS/2);
   chassis.pid_wait_until(-3);
@@ -637,7 +637,7 @@ void skills() {
   matchload.set(false);
   chassis.pid_turn_set(-45, TS);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set(-18, 127);
+  chassis.pid_odom_set(-20, 127);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(0, TS);
   chassis.pid_wait_quick_chain();
@@ -665,17 +665,17 @@ void skills() {
   intakeUS.move(0);
   chassis.pid_odom_set(5, DS);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set({{22, 24, -45}, fwd, DS});
+  chassis.pid_odom_set({{24, 24, -45}, fwd, DS});
   intakeUS.move(127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set(8, DS/2);
+  chassis.pid_odom_set(9, DS/2);
   intakeUS.move(-127);
   chassis.pid_wait_quick_chain();
   intakeLS.move(-80);
   pros::delay(2000);
   chassis.pid_odom_set(-2, DS);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set({{1, 10, 180}, fwd, DS});
+  chassis.pid_odom_set({{5, 10, 180}, fwd, DS});
   intakeLS.move(127);
   intakeUS.move(127);
   chassis.pid_wait_quick_chain();
