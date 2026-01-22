f64adc77a1e3f535ca78c894916460fca7b315a5
cohec <cohen.lu2000@gmail.com>
2026-01-22 13:38:52 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -713,7 +713,7 @@ void skills() {
   chassis.pid_odom_set(7, DS/3);
   chassis.pid_wait_quick_chain();
   pros::delay(300);
-  intakeLS.move(-80);
+  intakeLS.move(-60);
   pros::delay(2000);
   chassis.pid_odom_set(-5, DS);
   chassis.pid_wait_quick_chain();
@@ -778,7 +778,7 @@ void skills() {
   chassis.pid_odom_set(9, DS/2);
   intakeUS.move(-127);
   chassis.pid_wait_quick_chain();
-  intakeLS.move(-80);
+  intakeLS.move(-60);
   pros::delay(2000);
   chassis.pid_odom_set(-2, DS);
   chassis.pid_wait_quick_chain();
