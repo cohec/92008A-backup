da1b257b263dcd87d99a46951c97a464127e5507
cohec <cohen.lu2000@gmail.com>
2026-02-10 19:41:19 -0800

    bruh

--- src/autons.cpp
+++ src/autons.cpp
@@ -389,7 +389,7 @@ void auto_wp_left() {
   descore.set(true);
   chassis.drive_angle_set(-90);
   chassis.odom_xyt_set(0, 0, -90);
-  chassis.pid_odom_set(30, 127, true);
+  chassis.pid_odom_set(29, 127, true);
   chassis.pid_wait_until(10);
   matchload.set(true);
   chassis.pid_wait_quick_chain();
@@ -429,7 +429,7 @@ void auto_wp_left() {
   chassis.pid_odom_set({{50, 27, 90}, fwd, DS});
   intakeLS.move(127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(-2, DS/2);
+  chassis.pid_drive_set(-3, DS/2);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(-45, TS);
   chassis.pid_wait_quick_chain();
