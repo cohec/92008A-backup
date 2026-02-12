179aa0aea2153cdeb44cdfb6c87f12234277a6e3
cohec <cohen.lu2000@gmail.com>
2026-02-11 20:20:21 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -215,8 +215,8 @@ void ez_template_extras() {
     //  * use A and Y to increment / decrement the constants
     //  * use the arrow keys to navigate the constants
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
-      chassis.pid_tuner_toggle();
-    } if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+      chassis.pid_tuner_full_enable(true);
+    } else {
       chassis.pid_tuner_disable();
     }
     
