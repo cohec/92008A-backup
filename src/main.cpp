0525a7bb16efbfbcfc4145c62fe0ae68c19c470b
cohec <cohen.lu2000@gmail.com>
2026-02-11 20:10:47 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -216,8 +216,9 @@ void ez_template_extras() {
     //  * use the arrow keys to navigate the constants
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
       chassis.pid_tuner_toggle();
-    } else {
-      chassis.pid_tuner_disable();
+      if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+        chassis.pid_tuner_disable();
+      }
     }
 
     // Trigger the selected autonomous routine
