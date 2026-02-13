938e45e7d95c231450490385a68ad8dfb7352ea5
cohec <cohen.lu2000@gmail.com>
2026-02-13 11:35:21 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -47,7 +47,7 @@ void default_constants() {
 
   // The amount that turns are prioritized over driving in odom motions
   // - if you have tracking wheels, you can run this higher.  1.0 is the max
-  chassis.odom_turn_bias_set(0.9);
+  chassis.odom_turn_bias_set(0.95);
 
   chassis.odom_look_ahead_set(10_in);           // This is how far ahead in the path the robot looks at
   chassis.odom_boomerang_distance_set(16_in);  // This sets the maximum distance away from target that the carrot point can be
