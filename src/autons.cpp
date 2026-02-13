e6f6285b3ec385dd95c5a2a6ee255306e86ce3f1
cohec <cohen.lu2000@gmail.com>
2026-02-13 11:01:54 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -53,7 +53,7 @@ void default_constants() {
   chassis.odom_boomerang_distance_set(16_in);  // This sets the maximum distance away from target that the carrot point can be
   chassis.odom_boomerang_dlead_set(0.625);     // This handles how aggressive the end of boomerang motions are
 
-  chassis.pid_angle_behavior_set(ez::shortest);  // Changes the default behavior for turning, this defaults it to the shortest path there
+  chassis.pid_angle_behavior_set(ez::shortest);  // Changes the default behavior for turning, this defaults it to the shortest path
 }
 
 ///
