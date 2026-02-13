dc42d8a6df4d5e91ea849760ae562bf830572f7d
cohec <cohen.lu2000@gmail.com>
2026-02-13 14:34:05 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -25,17 +25,17 @@ void default_constants() {
   // P, I, D, and Start I
   chassis.pid_drive_constants_set(15.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(6.0, 0.0, 40.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(3.0, 0.05, 35.0, 3.0);     // Turn in place constants
+  chassis.pid_turn_constants_set(3.0, 0.05, 35.0, 10.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
   chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
 
   // Exit conditions
-  chassis.pid_turn_exit_condition_set(50_ms, 4_deg, 100_ms, 7_deg, 500_ms, 500_ms);
-  chassis.pid_swing_exit_condition_set(50_ms, 3_deg, 100_ms, 7_deg, 500_ms, 500_ms);
-  chassis.pid_drive_exit_condition_set(50_ms, 1.5_in, 100_ms, 3_in, 500_ms, 500_ms);
-  chassis.pid_odom_turn_exit_condition_set(30_ms, 4_deg, 100_ms, 7_deg, 500_ms, 750_ms);
-  chassis.pid_odom_drive_exit_condition_set(30_ms, 2_in, 100_ms, 3_in, 500_ms, 750_ms);
+  chassis.pid_turn_exit_condition_set(50_ms, 4_deg, 150_ms, 7_deg, 500_ms, 500_ms);
+  chassis.pid_swing_exit_condition_set(50_ms, 3_deg, 150_ms, 7_deg, 500_ms, 500_ms);
+  chassis.pid_drive_exit_condition_set(50_ms, 1.5_in, 150_ms, 3_in, 500_ms, 500_ms);
+  chassis.pid_odom_turn_exit_condition_set(30_ms, 4_deg, 150_ms, 7_deg, 500_ms, 750_ms);
+  chassis.pid_odom_drive_exit_condition_set(30_ms, 2_in, 150_ms, 3_in, 500_ms, 750_ms);
   chassis.pid_turn_chain_constant_set(3_deg);
   chassis.pid_swing_chain_constant_set(5_deg);
   chassis.pid_drive_chain_constant_set(3_in);
