f1f6a97cd9ee22d3a44f4c096ed18cf451f3c57f
cohec <cohen.lu2000@gmail.com>
2026-02-13 14:43:35 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -25,7 +25,7 @@ void default_constants() {
   // P, I, D, and Start I
   chassis.pid_drive_constants_set(15.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(6.0, 0.0, 40.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(3.0, 0.05, 35.0, 10.0);     // Turn in place constants
+  chassis.pid_turn_constants_set(3.0, 0.05, 40.0, 20.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
   chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
@@ -33,9 +33,9 @@ void default_constants() {
   // Exit conditions
   chassis.pid_turn_exit_condition_set(50_ms, 4_deg, 150_ms, 7_deg, 500_ms, 500_ms);
   chassis.pid_swing_exit_condition_set(50_ms, 3_deg, 150_ms, 7_deg, 500_ms, 500_ms);
-  chassis.pid_drive_exit_condition_set(50_ms, 1.5_in, 150_ms, 3_in, 500_ms, 500_ms);
-  chassis.pid_odom_turn_exit_condition_set(30_ms, 4_deg, 150_ms, 7_deg, 500_ms, 750_ms);
-  chassis.pid_odom_drive_exit_condition_set(30_ms, 2_in, 150_ms, 3_in, 500_ms, 750_ms);
+  chassis.pid_drive_exit_condition_set(50_ms, 2_in, 150_ms, 3_in, 500_ms, 500_ms);
+  chassis.pid_odom_turn_exit_condition_set(50_ms, 4_deg, 150_ms, 7_deg, 500_ms, 750_ms);
+  chassis.pid_odom_drive_exit_condition_set(50_ms, 2_in, 150_ms, 3_in, 500_ms, 750_ms);
   chassis.pid_turn_chain_constant_set(3_deg);
   chassis.pid_swing_chain_constant_set(5_deg);
   chassis.pid_drive_chain_constant_set(3_in);
@@ -47,7 +47,7 @@ void default_constants() {
 
   // The amount that turns are prioritized over driving in odom motions
   // - if you have tracking wheels, you can run this higher.  1.0 is the max
-  chassis.odom_turn_bias_set(0.9);
+  chassis.odom_turn_bias_set(0.6);
 
   chassis.odom_look_ahead_set(10_in);           // This is how far ahead in the path the robot looks at
   chassis.odom_boomerang_distance_set(16_in);  // This sets the maximum distance away from target that the carrot point can be
