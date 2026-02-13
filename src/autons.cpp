cfe4b3f8afb9a233bf8e9fd962ddc8d8354d837c
cohec <cohen.lu2000@gmail.com>
2026-02-13 14:24:46 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -23,9 +23,9 @@ const int SS = 110;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(12.0, 0.002, 120.0);         // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(15.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(6.0, 0.0, 40.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(2.5, 0.01, 35.0, 3.0);     // Turn in place constants
+  chassis.pid_turn_constants_set(3.0, 0.0, 35.0, 3.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
   chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
