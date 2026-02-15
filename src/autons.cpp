24fdb7e2a2abe506779e1c7d51cb38eccd19a2ee
cohec <cohen.lu2000@gmail.com>
2026-02-14 21:55:44 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -471,6 +471,8 @@ void auto_wp_right() {
   chassis.pid_wait_quick_chain();
   chassis.pid_swing_set(ez::RIGHT_SWING, -180, 127, 55, ccw);
   chassis.pid_wait_quick_chain();
+  chassis.pid_turn_set(-180, 127);
+  chassis.pid_wait_quick_chain();
   chassis.pid_odom_set(-7, 127);
   chassis.pid_wait_until(-3);
   intakeUS.move(127);
