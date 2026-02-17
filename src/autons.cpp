3f0a70e8c7b4dac0d1589664f51429261f72a4cf
cohec <cohen.lu2000@gmail.com>
2026-02-16 18:13:11 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -547,7 +547,7 @@ void left_sweep() {
   chassis.pid_swing_set(ez::LEFT_SWING, 0, 127, 10, cw);
   chassis.pid_wait_quick_chain();
   descore.set(false);
-  chassis.pid_swing_set(ez::LEFT_SWING, 20, 127, 20, cw);
+  chassis.pid_swing_set(ez::LEFT_SWING, 10, 127, 20, cw);
   chassis.pid_wait_quick_chain();
 }
 
