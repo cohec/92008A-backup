7f64029eccb446a82a72563135e98aa85f1ac2ba
cohec <cohen.lu2000@gmail.com>
2026-02-18 19:45:37 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -669,7 +669,7 @@ void right_contest() {
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(90, 127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_swing_set(ez::RIGHT_SWING, -10, 100, 10, ccw);
+  chassis.pid_swing_set(ez::RIGHT_SWING, -10, 100, 7, ccw);
   chassis.pid_wait_quick_chain();
   descore.set(false);
   chassis.pid_odom_set(15, 127);
