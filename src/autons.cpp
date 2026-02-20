320b604b57e3b465dca16cb86a0605796cf34aaf
cohec <cohen.lu2000@gmail.com>
2026-02-19 17:21:08 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -693,7 +693,7 @@ void skills() {
   chassis.pid_drive_set(20, 70);
   chassis.pid_wait_quick_chain();
   aligner.set(false);
-  chassis.pid_drive_set(10, 50);
+  chassis.pid_odom_set(10, 50);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(90, 127);
   chassis.pid_wait_quick_chain();
