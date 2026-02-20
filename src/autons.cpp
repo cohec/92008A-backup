8dbd105551d23f2ade5f297a76ed9c757c68dea9
cohec <cohen.lu2000@gmail.com>
2026-02-20 01:21:03 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -700,7 +700,7 @@ void skills() {
   aligner.set(false);
   chassis.pid_odom_set(10, 127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_swing_set(RIGHT_SWING, 180, -127, 0, cw);
+  chassis.pid_swing_set(RIGHT_SWING, 180, -127, 5, cw);
   chassis.pid_wait_quick_chain();
   chassis.pid_odom_set(10, DS/3);
   chassis.pid_wait_quick_chain();
