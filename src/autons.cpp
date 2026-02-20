cdb2013cdcb17fc00a4eecb3c1adba8bb4689f34
cohec <cohen.lu2000@gmail.com>
2026-02-19 21:13:31 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -687,7 +687,7 @@ void skills() {
   intakeUS.move(-127);
   intakeLS.move(127);
   chassis.pid_drive_set(40, 127);
-  chassis.pid_wait_until(10);
+  chassis.pid_wait_until(15);
   matchload.set(true);
   chassis.pid_wait_quick_chain();
   chassis.pid_drive_set(15, 70);
