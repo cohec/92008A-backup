ff86a891c99c396d7689f0da85591040aa358a7f
cohec <cohen.lu2000@gmail.com>
2026-03-01 11:15:21 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -585,8 +585,8 @@ void right_sweep() {
   chassis.pid_turn_set(-45, 127);
   chassis.pid_wait_quick_chain();
   matchload.set(false);
-  chassis.pid_odom_set(17, 127);
-  chassis.pid_wait_until(14);
+  chassis.pid_odom_set(15, 127);
+  chassis.pid_wait_until(10);
   intakeLS.move(-127);
   chassis.pid_wait_quick_chain();
   pros::delay(1500);
