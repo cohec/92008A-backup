8fc6cbeb32db107623dffee1f82dc83533ac5209
cohec <cohen.lu2000@gmail.com>
2026-03-01 13:44:28 -0800

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -590,7 +590,7 @@ void right_sweep() {
   intakeLS.move(-127);
   chassis.pid_wait_quick_chain();
   pros::delay(1500);
-  chassis.pid_odom_set(-2, 127);
+  chassis.pid_odom_set(-1, 127);
   chassis.pid_wait_quick_chain();
   chassis.pid_swing_set(LEFT_SWING, -180, -127, 20, ccw);
   chassis.pid_wait_quick_chain();
