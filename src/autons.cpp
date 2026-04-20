e76057f64d237db46f645529c9c73fe1e1519fab
cohec <cohen.lu2000@gmail.com>
2026-04-19 22:13:24 -0700

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -403,7 +403,7 @@ void test() {
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(135, 127);
   chassis.pid_wait_quick_chain();
-  chassis.pid_odom_set(-17, 127);
+  chassis.pid_odom_set(-15, 127);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(-135, 127);
   chassis.pid_wait_quick_chain();
